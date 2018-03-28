f7966c5a7ec84c38d6abd3fbedcda017b1654a0c
Daniel <danield9tqh@me.com>
2018-03-27 23:14:18 -0700

    added right to left trees as well

--- SVGTree.elm
+++ SVGTree.elm
@@ -10,16 +10,29 @@ canvas : Int -> Html.Html msg
 canvas numBranches =
     svg [width "100%", height "90%", version "1.1" ]
       [ {-- fillRect, --}
-       Svg.g [stroke "none", strokeWidth "1", fill "none", fillRule "evenodd", strokeLinecap "round"] (branches numBranches 150 350)
+       Svg.g [stroke "none", strokeWidth "1", fill "none", fillRule "evenodd", strokeLinecap "round"] (branchesRightToLeft numBranches 550 350)
       ]
 
 fillRect = rect [width "100%", height "90%", fill "red"] []
 
-branches n x y = case n of
+branchesLeftToRight n x y = case n of
   0 -> []
-  _ -> [uniformBranch x y] ++ branches (n-1) (x+50) (y-60)
+  _ -> [uniformBranchLeftToRight x y] ++ branchesLeftToRight (n-1) (x+50) (y-60)
 
-uniformBranch x y = let
+branchesRightToLeft n x y = case n of
+  0 -> []
+  _ -> [uniformBranchRightToLeft x y] ++ branchesRightToLeft (n-1) (x-50) (y-60)
+
+uniformBranchRightToLeft x y = let
+    branchHorizontalSpread = -50
+    branchVerticalSpread = 100
+    lowerBranchLength = 10
+    upperBranchLength = 50
+    branchCurvature = 15
+  in
+    createBranch x y branchHorizontalSpread branchVerticalSpread lowerBranchLength upperBranchLength branchCurvature "5" "#BD10E0"
+
+uniformBranchLeftToRight x y = let
     branchHorizontalSpread = 50
     branchVerticalSpread = 100
     lowerBranchLength = 10
