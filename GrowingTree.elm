64a7b5b6e8633cbda3f325a43955e62c20efe3d3
Daniel <danield9tqh@me.com>
2018-08-07 22:31:48 -0700

    added instructions to GrowingTree.elm

--- GrowingTree.elm
+++ GrowingTree.elm
@@ -5,11 +5,21 @@ import ParseInt exposing (parseInt)
 import SVGTree exposing (svgUniformTree)
 import Keyboard
 import Html.Events exposing (onWithOptions)
+import InstructionsSVG exposing (instructions)
 
-type alias Model = Int
+type alias Model = {
+  numBranches : Int,
+  showInstructions : Bool
+}
+
+newModel : Int -> Bool -> Model
+newModel numBranches showInstructions =
+  { numBranches = numBranches
+  , showInstructions = showInstructions
+  }
 
 init : (Model, Cmd Msg)
-init = (0, Cmd.none)
+init = (newModel 0 True, Cmd.none)
 
 type Msg
   = Change String
@@ -19,20 +29,30 @@ update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Change str -> case (parseInt str) of
-      Ok num -> (num, Cmd.none)
-      Err _  -> (0, Cmd.none)
+      Ok num -> (newModel num False, Cmd.none)
+      Err _  -> (newModel 0   False, Cmd.none)
     KeyMsg code -> case code of
-      38 -> (model + 1, Cmd.none)
-      40 -> (Basics.max (model - 1) 0, Cmd.none)
+      38 -> (newModel (model.numBranches + 1)                False , Cmd.none)
+      40 -> (newModel (Basics.max (model.numBranches - 1) 0) False , Cmd.none)
       _  -> (model, Cmd.none)
 
 subscriptions : Model -> Sub Msg
 subscriptions model = Keyboard.downs KeyMsg
 
 view : Model -> Html Msg
-view model = Html.div [Html.Attributes.style [("height", "100%"), ("width", "100%")] ]
-    [ Html.input [ defaultValue (toString model), placeholder "Natural Number", onInput Change ] []
-    , (svgUniformTree model 100)
+view model = Html.div [ Html.Attributes.style
+    [ ("height", "100%")
+    , ("width", "100%")
+    , ("display", "flex")
+    , ("flex-direction", "column")
+    , ("align-items", "center")
+    , ("justify-content", "center")
+    ] ]
+    [ Html.input [ defaultValue (toString model.numBranches), placeholder "Natural Number", onInput Change ] []
+    , ( case model.showInstructions of
+          True -> instructions
+          False -> (svgUniformTree model.numBranches 100)
+      )
     ]
 
 main =
